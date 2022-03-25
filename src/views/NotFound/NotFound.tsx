e780417818a5bc897446f6c0d19a6a325e8892de
Tom Zidani <tom.zidani@gmail.com>
2022-03-25 14:07:39 +0100

    Integrate not found page.

--- src/views/NotFound/NotFound.tsx
+++ src/views/NotFound/NotFound.tsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom"
+
 const NotFound = () => {
   return (
     <main className="app-notfound">
-      <div className="notfound__wrapper">404</div>
+      <div className="notfound__wrapper">
+        <h1 className="notfound__title">404</h1>
+        <p className="notfound__text">Oups ! La page que vous demandez n'existe pas.</p>
+        <Link to="/" className="notfound__link">
+          Retourner sur la page d'accueil
+        </Link>
+      </div>
     </main>
   )
 }
